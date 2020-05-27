ca10fbda41ccc4fcb76e3d65442707f7f513fff9
Oscar-Lopez-Projects <d1lopeztv@gmail.com>
2020-05-27 14:51:14 -0700

    working on adding features

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,26 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
+import {removeItems } from '../action/index';
 import AddedFeature from './AddedFeature';
 
 const AddedFeatures = props => {
+
+  const removeItems = (e, item) => {
+    e.preventDefault()
+    props.removeItems(item)
+  }
+
   return (
     <div className="content">
       <h6>Added features:</h6>
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature 
+            key={item.id} 
+            feature={item}
+            removeItems={e => removeItems(e, item)}
+             />
           ))}
         </ol>
       ) : (
@@ -19,4 +30,11 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+//set up mapStateProps and connect it
+const mapStateProps = state => {
+  return{
+    car:state.car
+  }
+}
+
+export default connect(mapStateProps, {removeItems})(AddedFeatures);
